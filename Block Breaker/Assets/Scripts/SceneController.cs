ab923bfd8d24da26cb87afe23fc71bfb9a323292
Juan Insuasti <juan.insuasti@gmail.com>
2019-11-25 12:55:32 -0500

    initial scene boilerplate

---
+++ Block Breaker/Assets/Scripts/SceneController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+
+    public void LoadNextScene() {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene + 1);
+    }
+
+    public void LoadFirstScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
