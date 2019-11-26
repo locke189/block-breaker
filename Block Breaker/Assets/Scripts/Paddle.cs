d1e493c614862547c228d74450af3b7e84a773a4
Juan Insuasti <juan.insuasti@gmail.com>
2019-11-26 09:26:29 -0500

    created first level

---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float paddleWidth = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xPos = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        xPos = Mathf.Clamp(xPos, 0, screenWidthInUnits - paddleWidth);
+        transform.position = new Vector2(xPos, transform.position.y);
+    }
+}
---
+++ Block Breaker/Assets/Scripts/SpikedBall.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikedBall : MonoBehaviour
+{
+    [SerializeField] Paddle paddle0;
+    [SerializeField] float verticalOffset = 1f;
+    [SerializeField] float horizontalOffset = 1.5f;
+    [SerializeField] float initialSpeed = 12f;
+    bool hasStarted;
+
+    void Start()
+    {
+        initializeBall();
+    }
+
+
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            SetAttachedPosition();
+            if (Input.GetMouseButtonDown(0)) {
+                LaunchBall();
+                hasStarted = true;
+            }
+        }
+    }
+
+    void initializeBall() {
+        hasStarted = false;
+    }
+
+    void SetAttachedPosition() {
+        Vector2 offset = new Vector2(horizontalOffset, verticalOffset);
+        Vector2 paddlePos = new Vector2(paddle0.transform.position.x, paddle0.transform.position.y);
+        transform.position = paddlePos + offset;
+    }
+
+    void LaunchBall() {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, initialSpeed);
+    }
+}
